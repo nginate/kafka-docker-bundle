7c6a0a62912c8b63c6f2c23772167f3b3a97d1d2
Oleksii Ignachuk <oignachuk@cybervisiontech.com>
2016-04-27 19:30:57 +0300

    setting zk url

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,10 +45,10 @@ log(){
             echo "can't create chroot in zookeeper, exit"
             exit 1
         }
-        sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181\/$ZK_CHROOT/g" ${KAFKA_HOME}/config/server.properties
+        sed -r -i "s/(zookeeper.connect)=(.*)/\1=${ADVERTISED_HOST}:2181\/$ZK_CHROOT/g" ${KAFKA_HOME}/config/server.properties
     } || {
         # configure kafka
-        sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181/g" ${KAFKA_HOME}/config/server.properties
+        sed -r -i "s/(zookeeper.connect)=(.*)/\1=${ADVERTISED_HOST}:2181/g" ${KAFKA_HOME}/config/server.properties
     }
 
     # Setting default log level for kafka
